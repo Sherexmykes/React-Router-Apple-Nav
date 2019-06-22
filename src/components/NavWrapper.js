a71d135c8b0d0bbf3e364038397ef35de95b99a8
Sheila R Moore <sherexmykes@github.com>
2019-06-22 14:52:25 -0400

    attempted stretch apple-nav

--- src/components/NavWrapper.js
+++ src/components/NavWrapper.js
@@ -6,31 +6,41 @@ import SubNav from './SubNav';
 import { Route } from 'react-router-dom';
 
 const StyledNavWrapper = styled.div`
-  width: 100%;
+max-width: 100%;
   background-color: #323232;
   color: white;
-  height: 40px;
+  height: 20px;
   display: flex;
-  justify-content: space-around;
-  padding: 10px;
-  position: fixed;
-  `;
-  const NavWrapper = props => {
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10%;
+  img {
+  height: 3rem;
+  padding-top: 0;
+  }
+`;
+const NavWrapper = props => {
     return (
       <div>
-      {props.data.map(d => (
-          <Route path={`/${d.name}`}
+        <StyledNavWrapper>
+          <img src="https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg" alt="apple"/>
+          {props.data.map(d => (
+            <Nav key={d.name} name={d.name} subLinks={d.subLinks} />
+          ))}
+          <Nav key='support' name='Support' />
+          <img src="https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg" alt="search"/>
+        </StyledNavWrapper>
+        {props.data.map(d => (
+          <Route key={d.name}
+            path={`/${d.name}`}
             render={props => <SubNav {...props} subLinks={d.subLinks} />}
           />
-      ))}
-   <StyledNavWrapper>
-   {props.data.map(d => (
-        <Nav name={d.name} subLinks={d.subLinks} />
-      ))}
-      </StyledNavWrapper>
+        ))}
+        <h2>Twice As Nice</h2>
       </div>
-  )
-}
+    );
+  };
+  
 
 export default NavWrapper;
 
--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSubNav = styled.div`
-  width: 50%;
-  color: #FAFAFA;
-  border: 1px solid red;
+max-width: 70%;
+padding: 10px;
+background-color: #black;
+color: black;
+  
   display: flex;
   justify-content: space-between;
   margin: 0 auto;
@@ -15,18 +17,21 @@ const SubItem = styled.div`
   flex-direction: column;
 `;
 
+
+
+
+
 const SubNav = props => {
   return (
     <StyledSubNav>
     {props.subLinks.map(sub => (
-      <SubItem>
-        <img src={sub.img} alt={sub.name} />
-        <p>{sub.name}</p>
-      </SubItem>
-    ))
-    }
-  </StyledSubNav>
-  )
-}
+        <SubItem key={sub.name}>
+          <img src={sub.img} alt={sub.name} />
+          <p>{sub.name}</p>
+        </SubItem>
+      ))}
+    </StyledSubNav>
+  );
+};
 
 export default SubNav;
